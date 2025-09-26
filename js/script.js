59ea34212f77a3529db518ceb94d7750e9128f85
Nim Carbonell Mas <ncarbonell.shifta@elisava.net>
2025-09-26 17:40:17 +0200

    20250926_Mapa:cards_organització

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 // ACTIVAR CLASSE SCROLLED PARA EL HEADER
 
 window.addEventListener("scroll", function() { //el navegador esta pendiente de cuando hay scroll
-  const header = document.querySelector("header"); //se guarda el elemento header en una constante, se usa querySelector porque solo elije el primer elemento y de header solo hay uno
+  const headerInicio = document.querySelector(".header-inicio"); //se guarda el elemento header en una constante, se usa querySelector porque solo elije el primer elemento y de header solo hay uno
   
   if (window.scrollY > 100) {  // cuantos pixeles tiene que haber bajado el header para cambiar
-      header.classList.add("scrolled"); //añade la classe scrolled al header, si se cumple la parte anterior de la función
+      headerInicio.classList.add("scrolled"); //añade la classe scrolled al header, si se cumple la parte anterior de la función
   } else {
-      header.classList.remove("scrolled"); //si no ha bajado más de los pixeles, elimina la clase. Al volver a la parte superior el fondo desaparece
+      headerInicio.classList.remove("scrolled"); //si no ha bajado más de los pixeles, elimina la clase. Al volver a la parte superior el fondo desaparece
   }
 });
 
